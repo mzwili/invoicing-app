c04e57703b977a3cc49d1932d4b987571cc6afc0
Mzwili <mthiyanemzwili@gmail.com>
2024-10-31 16:33:12 +0200

    form complete

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button"
-import Link from 'next/link';
 import { Textarea } from "@/components/ui/textarea"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
