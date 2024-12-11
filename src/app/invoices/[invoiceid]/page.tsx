875a31d7f68ddb01756f50eb12e7d22de323a2f5
Mzwili <mthiyanemzwili@gmail.com>
2024-12-11 15:29:30 +0200

    created errors page

--- src/app/invoices/[invoiceid]/page.tsx
+++ src/app/invoices/[invoiceid]/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import { notFound } from 'next/navigation';
 import { db } from '@/database'
 import { Invoices } from "@/database/schema";
 import { eq } from 'drizzle-orm';
@@ -9,10 +9,18 @@ export default async function InvoicePage({ params }: { params: Promise<{ invoic
   const { invoiceId } = await params;
   const invoiceIdNumber = parseInt(invoiceId);
 
+  if (isNaN(invoiceIdNumber)){
+    throw new Error('Invalid Invoice ID')
+  }
+
   const [result] = await db.select()
     .from(Invoices)
     .where(eq(Invoices.id, invoiceIdNumber))
     .limit(1)
+  
+  if(!result){
+    notFound();
+  }
 
   return (
     <main className="h-full max-w-5xl mx-auto my-12">
