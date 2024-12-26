d78a010caeba6783c1386db72ba550666e413fb1
Mzwili <mthiyanemzwili@gmail.com>
2024-12-26 12:13:11 +0200

    upgraded sign-in page

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtected = createRouteMatcher([
   '/',
-  
+  '/sign-in(.*)',
+  '/sign-up(.*)'
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
