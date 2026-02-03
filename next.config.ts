f54faa9e47d6f41214e199b8795b9ac662cb9649
mzwili <mthiyanemzwili@gmail.com>
2026-02-04 01:11:37 +0200

    updated modules

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    typedRoutes: true
-  }
-}
+import type { NextConfig } from "next";
 
-export default nextConfig
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+};
+
+export default nextConfig;
