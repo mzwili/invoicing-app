95057ddb89983ce9e10093c19762719564b62ca9
mzwili <mthiyanemzwili@gmail.com>
2026-02-04 07:59:40 +0200

    updated drizzle packaged

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,15 @@
-import * as dotenv from 'dotenv';
-import { defineConfig } from 'drizzle-kit';
-
-dotenv.config({
-    path: './.env.local'
-})
+import 'dotenv/config'
+import { defineConfig } from 'drizzle-kit'
 
 if(typeof process.env.XATA_DATABASE_URL !== 'string'){
     throw new Error('Please set your database url')
 }
 
 export default defineConfig({
-  out: './src/database/migrations',
-  schema: './src/database/schema.ts',
-  dialect: 'postgresql',
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dialect: 'mysql', // or 'postgresql' / 'sqlite'
   dbCredentials: {
-    url: process.env.XATA_DATABASE_URL
-  }
-});
+    url: process.env.DATABASE_URL!,
+  },
+})
